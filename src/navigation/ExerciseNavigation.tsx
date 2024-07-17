4b4b44a1e8b2d377a163179749992bc6597ea4d5
charles <nischalcharles@outlook.com>
2024-07-17 01:36:58 -0500

    Refactor navigation and screens; introduce ExerciseDetail

--- src/navigation/ExerciseNavigation.tsx
+++ src/navigation/ExerciseNavigation.tsx
@@ -1,18 +1,20 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { EXERCISE_EDIT, EXERCISE_FORM, EXERCISE_LIST, PROFILE } from "../constants/screenNames";
-import ExerciseList from "../components/ExcerciseList";
+import { EXERCISE_DETAILS, EXERCISE_EDIT, EXERCISE_FORM, EXERCISE_LIST } from "../constants/screenNames";
+import ExerciseList from "../components/ExerciseList";
 import ExerciseForm from "../components/ExerciseForm";
-import EditExerciseScreen from "../screens/EditExcercise";
+import EditExercise from "../screens/EditExcercise";
+import ExcerciseDetails from "../screens/ExerciseDetails";
 
 
 const ExerciseTabs = createNativeStackNavigator();
 
 export default function ExerciseNavigation() {
     return (
-        <ExerciseTabs.Navigator screenOptions={{headerShown: false}} initialRouteName={PROFILE}>
+        <ExerciseTabs.Navigator screenOptions={{headerShown: false}} initialRouteName={EXERCISE_LIST}>
             <ExerciseTabs.Screen name={EXERCISE_LIST} component={ExerciseList} />
             <ExerciseTabs.Screen name={EXERCISE_FORM} component={ExerciseForm}  />
-            <ExerciseTabs.Screen name={EXERCISE_EDIT} component={EditExerciseScreen} />
+            <ExerciseTabs.Screen name={EXERCISE_EDIT} component={EditExercise} />
+            <ExerciseTabs.Screen name={EXERCISE_DETAILS} component={ExcerciseDetails} />
         </ExerciseTabs.Navigator>
     );
 }
