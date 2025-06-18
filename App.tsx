33252f88e5e464ec8165ddb89f63296742abca51
nischal <nischalcharles@outlook.com>
2025-06-18 00:52:05 -0400

    fix: Update import path for GluestackUIProvider and include components directory in TypeScript configuration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
